0c02426df2d472ad664ad95b7fa03ebbeb72a1b9
erkutbas <ebas1@binghamton.edu>
2019-06-06 09:03:10 +0300

    Initial Commit

---
+++ iOS_MyTaxi_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_MyTaxi_Challenge
+//
+//  Created by Erkut Baş on 6/6/19.
+//  Copyright © 2019 Erkut Baş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
